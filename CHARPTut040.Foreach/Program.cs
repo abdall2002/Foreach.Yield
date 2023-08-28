85fa610d31273c577ce345ba6aafedea66e4301c
unknown <abdallahaliahmed517@gmail.com>
2023-08-28 11:44:01 +0300

    FFF

---
+++ CHARPTut040.Foreach/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var numbers = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+Console.WriteLine("\n \n Using For: ");
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($" {numbers[i]}");
+}
+
+Console.WriteLine("\n\n Using Foreach");
+
+foreach (var n in numbers)
+{
+    Console.Write($" {n}");
+}
